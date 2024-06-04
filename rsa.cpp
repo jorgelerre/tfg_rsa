94a4f9b7c0a9b1246f972e0b228e412c6b6eae9c
Jorge López <jorgelremacho2@gmail.com>
2024-06-04 18:53:02 +0200

    RSA + Primos robustos

--- rsa.cpp
+++ rsa.cpp
@@ -118,16 +118,115 @@ bool millerRabin(mpz_t p, int it, gmp_randstate_t state){
 
 
 // Función para generar un número primo de 'bits' bits
-void generate_prime(mpz_t prime, int bits) {
-	gmp_randstate_t gmp_randstate;
-	
-    mpz_rrandomb(prime, gmp_randstate, bits); // Genera un número aleatorio de 'bits' bits
-    mpz_nextprime(prime, prime); // Encuentra el siguiente número primo
+void generate_prime(mpz_t p, int bits, gmp_randstate_t state) {
+    mpz_rrandomb(p, state, bits); // Genera un número aleatorio de 'bits' bits
+    //Si el numero es par, le sumamos 1
+    if(!mpz_odd_p(p)){
+    	mpz_add_ui(p, p, 1);
+    }
+    //Mientras p no pase el test de Miller-Rabin, avanzamos al siguiente numero impar
+    while(!millerRabin(p, 10, state)){
+    	mpz_add_ui(p, p, 2);
+    }
+}
+
+// Función para generar un número primo robusto de 'bits' bits
+void generate_strong_prime(mpz_t p, int bits, gmp_randstate_t state) {
+    mpz_t p_0,r,s,t,i,j,aux;
+    
+    mpz_inits(p_0, r, s, t, i,j,aux, nullptr);
+    
+    //Generamos dos primos grandes s, t
+    generate_prime(s, bits, state);
+    generate_prime(t, bits, state);
+    
+    //Elegimos un numero aleatorio i
+    mpz_urandomb(i, state, bits);
+    
+    //Calculamos r: r sea primo
+    do{
+    	mpz_add_ui(i, i, 1);	//i++
+    	mpz_mul_ui(r, i, 2);	//r = 2*i*t + 1
+    	mpz_mul(r, r, t);
+    	mpz_add_ui(r, r, 1);
+    }while(!millerRabin(r,10,state));
+    
+    //Calculamos p_0
+    //p_0 = 2*s^{r-2} (mod r) *s - 1
+    mpz_sub_ui(p_0, r, 2);
+    mpz_powm(p_0, s, p_0, r);
+    mpz_mul_ui(p_0, p_0, 2);
+    mpz_fdiv_r(p_0, p_0, r);
+    mpz_mul(p_0, p_0, s);
+    mpz_sub_ui(p_0, p_0, 1);
+    
+    //Elegimos un numero aleatorio j
+    mpz_urandomb(j, state, bits);
+    //aux = 2rs
+    mpz_mul_ui(aux, r, 2);
+    mpz_mul(aux, aux, s);
+    //p = 2rsj + p_0
+    mpz_mul(p, aux, j);
+    mpz_add(p, p, p_0);
+    
+    //Mientras p no sea primo, le sumamos aux
+    while(!millerRabin(p,10,state)){
+    	mpz_add(p, p, aux);
+    }
+    
+    mpz_clears(p_0, r, s, t, i, j, nullptr);
 }
 
 // Función principal para generar claves RSA
-void generate_rsa_key(int bits, mpz_t n, mpz_t e, mpz_t d) {
+// TODO: Implementar parametro para elegir generacion de primos (robustos o no)
+void generate_rsa_key(int bits, mpz_t n, mpz_t e, mpz_t d, gmp_randstate_t state) {
+	mpz_t p, q, phi_n, mcd;
+	mpz_inits(p, q, phi_n, mcd, nullptr);
+	unsigned int e_default = 65535;
+	//Elegimos dos primos p y q aleatoriamente
+	generate_prime(p, bits, state);
+	generate_prime(q, bits, state);
 	
+	//Calculamos n y phi(n)
+	mpz_mul(n,p,q);		//n = p*q
+	mpz_sub_ui(p, p, 1);
+	mpz_sub_ui(q, q, 1);
+	mpz_mul(phi_n,p, q);
+	mpz_add_ui(p, p, 1);
+	mpz_add_ui(q, q, 1);
+	
+	//Elegimos el exponente de cifrado e = 2^16-1 = 65.535 si < phi_n
+	if(mpz_cmp_ui(phi_n, e_default) > 0){
+		mpz_set_ui(e, e_default);
+	}
+	//En otro caso, lo elegimos aleatoriamente (un bit mas pequeño que phi_n)
+	else{
+		mpz_urandomb(e, state, mpz_sizeinbase(phi_n, 2));
+		if(!mpz_odd_p(e)){
+			mpz_sub_ui(e, e, 1);
+		}
+	}
+	
+	mpz_gcd(mcd, e, phi_n);
+	mpz_sub_ui(mcd, mcd, 1);
+	
+	//Mientras e y phi_n no sean coprimos
+	while(mpz_cmp_ui(mcd, 0) > 0 && mpz_cmp_ui(e, 2) > 0 && mpz_cmp_ui(e, 2) > 0 ){
+		mpz_urandomb(e, state, mpz_sizeinbase(phi_n, 2));
+		if(!mpz_odd_p(e)){
+			mpz_sub_ui(e, e, 1);
+		}
+		
+		mpz_gcd(mcd, e, phi_n);
+		mpz_sub_ui(mcd, mcd, 1);
+	}
+	
+	//Calculamos el inverso de e en Z_phi(n) = d
+	mpz_invert(d, e, phi_n);
+	
+	//TODO: Comprobar ataque Wiener
+	
+	mpz_clears(p, q, phi_n, mcd, nullptr);
 }
 
 int main() {
@@ -138,12 +237,18 @@ int main() {
     
     //Programa principal
 	bool pseudoprimo;
+	long unsigned int r;
 	mpz_t p, base;
 	mpz_inits(p, base, nullptr);
 	mpz_set_ui(p, 503);
 	mpz_set_ui(base, 2);
 	pseudoprimo = millerRabin(p, 5, state);
-	cout << pseudoprimo << endl;
+	cout << "Pseudoprimo: " << pseudoprimo << endl;
+	
+	
+	generate_prime(p, 32, state);
+	r = mpz_getlimbn(p, 0);
+	cout << "Primo encontrado: " << r << endl;
 	
 	mpz_clears(p, base, state, nullptr);
     return 0;
